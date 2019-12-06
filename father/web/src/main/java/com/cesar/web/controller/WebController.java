2d7e3af9dcba034b051f6eba603152b1ea2d3f40
Cesar.Yuan <Cesar.Yuan@movit-tech.com>
2019-12-06 14:36:22 +0800

    add:增加swagger依赖

--- father/web/src/main/java/com/cesar/web/controller/WebController.java
+++ father/web/src/main/java/com/cesar/web/controller/WebController.java
@@ -1,6 +1,7 @@
-package com.cesar.web;
+package com.cesar.web.controller;
 
 import com.cesar.service.ServiceTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("test")
-public class WebTest {
+public class WebController {
 
+//    @Autowired
+//    private ServiceTest
     @GetMapping("showAll")
     public String showAll(){
         ServiceTest serviceTest = new ServiceTest();
