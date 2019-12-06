0c66d37fe0432f569598b2d86a144a2fe4bd84b1
Cesar.Yuan <Cesar.Yuan@movit-tech.com>
2019-12-06 14:56:59 +0800

    add:增加mybatis-plus

--- father/web/src/main/java/com/cesar/web/WebApplication.java
+++ father/web/src/main/java/com/cesar/web/WebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.cesar")
-@MapperScan("com.cesar.dao.mapper")
+@MapperScan("com.cesar.dao")
 public class WebApplication {
 
     public static void main(String[] args) {
